c14b0fa78ac772028410dbcda29ab8072f7a6794
Jose Castro <JoseAneurys@me.com>
2022-12-02 20:17:29 -0500

    fourth commit, Chase's freebieee

--- javascriptReviewLab.js
+++ javascriptReviewLab.js
@@ -340,3 +340,88 @@ if (aNumber < 5) {
 };
 console.log("");
 
+// H. What's in Your Closet?
+// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence 
+//"Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
+console.log("");
+
+// Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+kristynsCloset.splice(6, 0, "raybans");
+console.log(kristynsCloset);
+console.log("");
+
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset[5] = "stained knit hat";
+console.log(kristynsCloset);
+console.log("");
+
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+let thomsShirts = thomsCloset[0]; thomsCloset.splice(0,1,thomsShirts);
+let thomsPants = thomsCloset[1]; thomsCloset.splice(1,1,thomsPants);
+let thomsAccessories = thomsCloset[2]; thomsCloset.splice(2,1,thomsAccessories);
+console.log(thomsCloset);
+console.log("");
+console.log(thomsShirts[0]);
+console.log("");
+
+// In the same way, access one item from Thom's pants array.
+console.log(thomsPants[1]);
+console.log("");
+
+// Access one item from Thom's accessories array.
+console.log(thomsAccessories[2]);
+console.log("");
+
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log("Thom is slaying in a " + thomsShirts[0] + ", " + thomsPants[1] + ", & " + thomsAccessories[2] + "!");
+console.log("");
+
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsPants[2] = "Footie Pajamas";
+console.log(thomsCloset);
+console.log("");
+
+//IV. Functions
+
+// A. printGreeting
+// Do you think you could write a function called printGreetingwith a parameter namethat returns a greeting with the argument interpolated
+//into the greeting?
+// Like so?
+// console.log(printGreeting("Slimer"));
+// => Hello there, Slimer!
+// You think you could? I think so too. Feel free to skip this problem, because you've already done it.
+//If you've done the problem twice, read entire problems carefully before doing them from now on.
+console.log("FREEBIEEEEEE Thanks, Chase!");
+console.log("");
+
